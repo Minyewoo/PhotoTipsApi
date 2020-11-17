e87aebc1b38240576e53c076e1460a877440ecea
Minyewoo <const.trushov@gmail.com>
2020-11-17 15:31:21 +0300

    post-deploy fixes

--- PhotoTipsApi/Repositories/PhotoTipsDbContext.cs
+++ PhotoTipsApi/Repositories/PhotoTipsDbContext.cs
@@ -12,10 +12,14 @@ namespace PhotoTipsApi.Repositories
         public DbSet<LectureContent> LectureContents { get; set; }
         public DbSet<Photo> Photos { get; set; }
         
+        public PhotoTipsDbContext()
+        {
+            Database.EnsureCreated();
+        }
         
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=photo_tips;Username=postgres;Password=password");
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=photo_tips;Username=minyewoo;Password=wefunk2020");
         }
     }
     
