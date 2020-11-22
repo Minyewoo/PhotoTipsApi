f7ffe9d8418534ac24aefb2528600b84e99222d0
Konstantin Trushov <const.trushov@gmail.com>
2020-11-22 22:51:37 +0500

    Update PhotoTipsDbContext.cs

--- PhotoTipsApi/Repositories/PhotoTipsDbContext.cs
+++ PhotoTipsApi/Repositories/PhotoTipsDbContext.cs
@@ -20,7 +20,7 @@ namespace PhotoTipsApi.Repositories
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql(
-                "Host=85.208.208.156;Port=5432;Database=photo_tips;Username=minyewoo;Password=wefunk2020");
+                "Host=localhost;Port=5432;Database=photo_tips;Username=minyewoo;Password=wefunk2020");
         }
     }
 }
